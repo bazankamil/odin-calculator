945dd2b8034b06071e3b90c284c3218d76d68f64
bazankamil <bazankamil@gmail.com>
2023-09-21 13:57:00 +0200

    added buttons selectors, changed colors, added maintenance classes

---
+++ index.js
@@ -0,0 +1,35 @@
+/* SELECTORS */
+//Maintenance
+const buttonElementMRC = document.querySelector(".button-MRC");
+const buttonElementMPlus = document.querySelector(".button-MPlus");
+const buttonElementMMinus = document.querySelector(".button-MMinus");
+const buttonElementCE = document.querySelector(".button-CE");
+const buttonElementOnOff = document.querySelector(".button-ON-OFF");
+//Numbers
+const buttonElementZero = document.querySelector(".button-0");
+const buttonElementOne = document.querySelector(".button-1");
+const buttonElementTwo = document.querySelector(".button-2");
+const buttonElementThree = document.querySelector(".button-3");
+const buttonElementFour = document.querySelector(".button-4");
+const buttonElementFive = document.querySelector(".button-5");
+const buttonElementSix = document.querySelector(".button-6");
+const buttonElementSeven = document.querySelector(".button-7");
+const buttonElementEight = document.querySelector(".button-8");
+const buttonElementNine = document.querySelector(".button-9");
+//Rest
+const buttonElementPercentage = document.querySelector(".button-percentage");
+const buttonElementRoot = document.querySelector(".button-root");
+const buttonElementMultiplication = document.querySelector(
+  ".button-multiplication"
+);
+const buttonElementDivision = document.querySelector(".button-division");
+const buttonElementPlus = document.querySelector(".button-plus");
+const buttonElementMinus = document.querySelector(".button-minus");
+const buttonElementDot = document.querySelector(".button-dot");
+const buttonElementEmpty = document.querySelector(".button-empty");
+const buttonElementEqual = document.querySelector(".button-equal");
+
+/*TODO:
+-event listenery na click- update czytnika
+
+*/
