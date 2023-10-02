b8d4a211c577332b301b94323f28a152b9cdce7f
bazankamil <bazankamil@gmail.com>
2023-10-02 12:53:54 +0200

    calculator works in basic form, planned addition functionality

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ const buttonElementCE = document.querySelector(".button-CE");
 const buttonElementOnOff = document.querySelector(".button-ON-OFF");
 //Numbers
 const buttons = document.querySelectorAll(".button-number");
+//Operators
+const operators = document.querySelectorAll(".button-operator");
 //Rest
 const buttonElementPercentage = document.querySelector(".button-percentage");
 const buttonElementRoot = document.querySelector(".button-root");
-const buttonElementMultiplication = document.querySelector(
-  ".button-multiplication"
-);
-const buttonElementDivision = document.querySelector(".button-division");
-const buttonElementPlus = document.querySelector(".button-plus");
-const buttonElementMinus = document.querySelector(".button-minus");
 const buttonElementDot = document.querySelector(".button-dot");
 const buttonElementEmpty = document.querySelector(".button-empty");
 const buttonElementEqual = document.querySelector(".button-equal");
 const divElementResultScreen = document.querySelector(".result__screen");
 
-//Operator
-
-//Event listeners
+//Variables
+let variable1 = 0;
+let variable2;
+let operator;
+let result;
+//Event listeners button push
 
 buttons.forEach((item) => {
   item.addEventListener("click", function () {
@@ -31,15 +30,36 @@ buttons.forEach((item) => {
   });
 });
 
-//Variables
-let variable1;
-let variable2;
-let operator;
-let display;
+//Event listener operators
+
+operators.forEach((item) => {
+  item.addEventListener("click", function () {
+    operator = this.innerText;
+    variable1 = divElementResultScreen.innerText;
+    divElementResultScreen.innerText = "";
+  });
+});
+
+//Equal
+
+buttonElementEqual.addEventListener("click", function () {
+  if (variable1 != 0 && variable1 != "") {
+    variable2 = divElementResultScreen.innerText;
+    result = operate(variable1, variable2, operator);
+    if (result) {
+      divElementResultScreen.innerText = result;
+    }
+  }
+});
+
+//Clear
+buttonElementCE.addEventListener("click", function () {
+  divElementResultScreen.innerText = "";
+});
 
 //Calculator funcions
 function add(a, b) {
-  return a + b;
+  return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
@@ -55,20 +75,32 @@ function divide(a, b) {
 }
 
 function operate(variable1, variable2, operator) {
-  return function (variable1, variable2) {};
+  if (operator == "+") {
+    return add(variable1, variable2);
+  } else if (operator == "-") {
+    return subtract(variable1, variable2);
+  } else if (operator == "x") {
+    return multiply(variable1, variable2);
+  } else if (operator == "/") {
+    return divide(variable1, variable2);
+  }
 }
 
 /*TODO:
--Dodac logo (stanczyka i logo odina) a na prawo dwa guziki- work i nie work
-//wybrac dzialajace guziki, na reszte dac nie dzialajace kolory
-//query selector all na operatory, ale tylko dzialajace
-//zmienna operator na 0, ale kiedy nacisniety button to otrzymuje wartosc "add, substract etc"
-//kiedy nacisniety operator to wartosc wyswietlacza zapisywana w var1 a wyswietlacz resetowany
 
-//rowna sie
-//jesli nacisniete rowna sie to zawartosc result screen zapisywana w var 2,
-//display usuwany, wartosci przekazyane do funkcji i wyswietlany wynik
+-ciagi dzialan: jesli var 1 i var2 maja wartosc to kolejny operant dziala jak rowna sie i zapisuje wynik w var1 var1 rowna sie result
+-zaokragla do 15 po przecinku
+-jesli result wiekszy niz 15 liczb to albo ucinac albo komunikat, ze za duza liczba
+-rowna sie bez var1 i var2- nic nie robic powinno
+-rowna sie dziala tylko jak oba var maja wartosc
+-dzielenie przez zero powinno dac jakis komunikat smieszkowaty
+-dodac kropke i dzialania na ulamkach, ale kiedy jest juz jedna kropka nie pozwalaj na wiecej
+-zamienic pusty na backspace i usuwac ostatnia wpisana liczbe
+
 
 -klasy z numerami pousuwac
--
+-on/off dezaktywuje kalkulator (kolory wyszarza) i nie da sie nic kliknac procz on
+-niedzialajace guziki wyszarzyc
+
+-z czysta lista TODO- refaktor, pousuwac nieuzywany kod i udostepnic
 */
